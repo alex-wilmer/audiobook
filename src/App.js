1a03df201f99e9a8541f1a736746faaded212a18
Alex Wilmer <djazium@gmail.com>
2017-08-02 00:30:20 -0400

    🎶📸 day 1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,35 @@
 import React from 'react'
-export default () => <div />
+import load from 'audio-loader'
+import './app.css'
 
-console.log('123')
+let L = p =>
+  <div>
+    {p.on ? 'X' : 'O'}
+  </div>
+
+let ctx = new AudioContext()
+
+export default class extends React.Component {
+  state = { p1: null }
+  componentDidMount() {
+    load('/a1.wav').then(p1 => this.setState({ p1 }, this.play))
+  }
+  play = () => {
+    if (this.state.p1) {
+      let source = ctx.createBufferSource()
+      source.buffer = this.state.p1
+      source.connect(ctx.destination)
+      source.loop = true
+      source.start()
+    }
+  }
+  render() {
+    return (
+      <div>
+        <video autoPlay loop id="video-background" muted playsInline>
+          <source src="/water.mov" />
+        </video>
+      </div>
+    )
+  }
+}
