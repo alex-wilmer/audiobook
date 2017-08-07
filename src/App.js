e0eb24e70b428807b91e387a28bdcb0dab212b80
Alex Wilmer <djazium@gmail.com>
2017-08-06 21:47:33 -0400

    ✨ day 6

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ let L = p =>
 
 let ctx = new AudioContext()
 
-export default class extends React.Component {
+class A1 extends React.Component {
   state = { p1: null }
   componentDidMount() {
     load(process.env.PUBLIC_URL + '/a1.wav').then(p1 =>
@@ -31,6 +31,45 @@ export default class extends React.Component {
         <video autoPlay loop id="video-background" muted playsInline>
           <source src={process.env.PUBLIC_URL + '/Untitled.mov'} />
         </video>
+      </div>
+    )
+  }
+}
+
+class A2 extends React.Component {
+  state = { p1: null }
+  componentDidMount() {
+    load(process.env.PUBLIC_URL + '/a6v2.wav').then(p1 =>
+      this.setState({ p1 }, this.play)
+    )
+  }
+  play = () => {
+    if (this.state.p1) {
+      let source = ctx.createBufferSource()
+      source.buffer = this.state.p1
+      source.connect(ctx.destination)
+      source.loop = true
+      source.start()
+    }
+  }
+  render() {
+    return (
+      <div>
+        <video autoPlay loop id="video-background" muted playsInline>
+          <source src={process.env.PUBLIC_URL + '/v6.mov'} />
+        </video>
+      </div>
+    )
+  }
+}
+
+export default class extends React.Component {
+  state = { a: 1 }
+  componentDidMount() {}
+  render() {
+    return (
+      <div>
+        <A2 />
       </div>
     )
   }
