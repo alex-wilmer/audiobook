d2356dc000e4015fc3eff9ae34dbc8ffdc6453e6
Alex Wilmer <djazium@gmail.com>
2017-08-07 18:04:49 -0400

    🎥 ajax video

--- src/App.js
+++ src/App.js
@@ -3,11 +3,6 @@ import load from 'audio-loader'
 import WebAudioScheduler from 'web-audio-scheduler'
 import './app.css'
 
-let L = p =>
-  <div>
-    {p.on ? 'X' : 'O'}
-  </div>
-
 let ctx = new AudioContext()
 let sched = new WebAudioScheduler({ context: ctx })
 let masterGain = null
@@ -22,40 +17,55 @@ sched.on('stop', () => {
   masterGain = null
 })
 
-var secondsPerBeat = 60.0 / 100
+let secondsPerBeat = 60.0 / 100
+let p1L = secondsPerBeat * 24
+let p2L = secondsPerBeat * 48
+
+let wavs = ['water', 'weeds']
+
+let V = props =>
+  <video autoPlay loop className="video-background" muted playsInline>
+    <source src={props.src} />
+  </video>
 
 export default class extends React.Component {
-  state = { p1: null }
-  componentDidMount() {
-    load(process.env.PUBLIC_URL + '/water.wav').then(p1 =>
-      this.setState({ p1 })
+  state = {
+    loading: true,
+    buffers: [],
+    vid: null
+  }
+  async componentDidMount() {
+    let buffers = await Promise.all(
+      wavs.map(x => load(process.env.PUBLIC_URL + `/${x}.wav`))
     )
+    let v = await fetch(process.env.PUBLIC_URL + `/water.mov`).then(r =>
+      r.blob()
+    )
+    let vid = URL.createObjectURL(v)
+    this.setState({ buffers, loading: false, vid })
   }
   song = e => {
-    const t0 = e.playbackTime
-
-    sched.insert(t0 + 0.0, this.section)
-    sched.insert(t0 + secondsPerBeat * 24, this.song)
+    let t0 = e.playbackTime
+    sched.insert(t0 + 0.0, e => this.section(e, 0))
+    sched.insert(t0 + p1L, e => this.section(e, 1))
+    sched.insert(t0 + p1L + p2L, e => this.section(e, 0))
   }
-  section = e => {
-    const t0 = e.playbackTime
-    const amp = ctx.createGain()
-
+  section = (e, p) => {
+    let t0 = e.playbackTime
+    let amp = ctx.createGain()
     let source = ctx.createBufferSource()
-    source.buffer = this.state.p1
+    source.buffer = this.state.buffers[p]
     source.connect(ctx.destination)
     source.start(t0)
-
-    // amp.gain.setValueAtTime(0.5, t0);
-    // amp.gain.exponentialRampToValueAtTime(1e-6, t1);
     amp.connect(masterGain)
   }
-
   render() {
     return (
       <div>
+        {this.state.loading ? 'loading' : ''}
         <button onClick={() => sched.start(this.song)}>start</button>
         <button onClick={() => sched.stop(true)}>stop</button>
+        {this.state.vid && <V src={this.state.vid} />}
       </div>
     )
   }
